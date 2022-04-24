8567c2905ca18b75f4d17a302f1e0a468ffe7b66
chinhnguyen123 <chinhnguyen11032000@gmail.com>
2022-04-24 14:18:48 +0700

    lan thu 2 dua len server

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -19,6 +19,10 @@ public class MathUtilLive {
         System.out.println("expected: 5! = 120; actual: " + result);
         System.out.println("expected: 6! = 720; actual: " + MathUtil.getFactorial(6));
         System.out.println("expected: 0! = 1; actual: " + MathUtil.getFactorial(0));
+        
+        //thêm code sau lần đầu tiên up code lên server 2:16pm 24/4/2022
+        System.out.println("expected: 1! = 1; actual: " + MathUtil.getFactorial(1));
+        System.out.println("expected: 3! = 6; actual: " + MathUtil.getFactorial(3));
         //MathUtil.getFactorial(-5);
     }
     
